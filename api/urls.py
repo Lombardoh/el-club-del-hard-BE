7da046e2360679d5af7498dfc4409825e760cc7c
lombardoh <lombardo.h@hotmail.com>
2022-07-05 00:02:43 -0300

    initial

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from .views import ProductViewSet 
+
+router = routers.DefaultRouter()
+router.register("products", ProductViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
