08835d3b790cf3860015a36080cc2777b11829c5
lombardoh <lombardo.h@hotmail.com>
2022-10-17 17:10:42 -0300

    ordered products by name

--- store/admin.py
+++ store/admin.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from store.models import Product, Serial, Category
 
 class SerialInline(admin.TabularInline):
-    model = Serial
-    extra = 0
+  model = Serial
+  extra = 0
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+  pass
 
 class ProducAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'alt', 'description', 'price', 'image')
-    search_fields = ('pk', 'name', 'alt', 'description', 'price', 'image')
-    list_filter = ()
-    fieldsets = ()
-    inlines = [SerialInline]
+  list_display = ('pk', 'name', 'alt', 'description', 'price', 'image')
+  search_fields = ('pk', 'name', 'alt', 'description', 'price', 'image')
+  ordering = ('name',)
+  list_filter = ()
+  fieldsets = ()
+  inlines = [SerialInline]
 
 
 admin.site.register(Product, ProducAdmin)
